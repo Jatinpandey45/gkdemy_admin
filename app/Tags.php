4c71c98bf3d865bd208e87696eb2dcbe7f6f83af
Jatin <jay.pandey45@gmail.com>
2019-11-24 11:36:53 +0530

    updaet

--- app/Tags.php
+++ app/Tags.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+
 use App\Lang;
 
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +12,7 @@ class Tags extends Model
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
-    
+
     protected $table = "gk_tags";
 
     protected $fillable = [
@@ -28,27 +29,25 @@ class Tags extends Model
 
     public function post()
     {
-        return $this->belongsToMany('App\Posts','gk_tag_post','tag_id','post_id');
+        return $this->belongsToMany('App\Posts', 'gk_tag_post', 'tag_id', 'post_id');
     }
 
     public static function getAllTagRelatedPost()
-     {
+    {
 
-        return Tags::with('post.Category','post.Tags','post.Month','post.Seo')->paginate(10);
+        return Tags::with('post.Category', 'post.Tags', 'post.Month', 'post.Seo')->paginate(10);
+    }
 
-     }
 
-
-     public static function getAllTagRelatedPostById($id)
-     {
-        return Tags::where('id',$id)->with('post.Category','post.Tags','post.Month','post.Seo')->paginate(10);
-     }
+    public static function getAllTagRelatedPostById($id)
+    {
+        return Tags::with(['post.Category', 'post.Tags', 'post.Month', 'post.Seo', function ($q) { $q->paginate(10); }])->where('id', $id)->first();
+    }
 
     public static function searchTags($search)
     {
-        return Tags::
-        where('tag_name', 'LIKE', "%{$search}%")
-        ->orWhere('tag_slug', 'LIKE', "%{$search}%")
-        ->get();
+        return Tags::where('tag_name', 'LIKE', "%{$search}%")
+            ->orWhere('tag_slug', 'LIKE', "%{$search}%")
+            ->get();
     }
 }
