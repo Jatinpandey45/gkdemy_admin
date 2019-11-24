d7bd375f5a4aa1b6b7c015f2008611a0846b4e95
root <jay.pandey45@gmail.com>
2019-11-24 20:54:29 +0530

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>GkDemy New</title>
+        <title>GkDemy</title>
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
-    <body>
+    <body data-aos-easing="ease" data-aos-duration="1500" data-aos-delay="0" class="body-scrolled navbar-scrolled">
         <div id="root"></div>
         <script src="{{mix('js/app.js')}}" ></script>
     </body>
