e02af663b8e4918ee008f80a7d05868fc27185da
root <jay.pandey45@gmail.com>
2019-12-01 18:20:19 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>GkDemy</title>
+        <title>GKDemy</title>
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body data-aos-easing="ease" data-aos-duration="1500" data-aos-delay="0" class="body-scrolled navbar-scrolled">
